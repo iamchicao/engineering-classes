7d38eb2175912d05efffb844699c79c9b075155c
Francisco Junior <jr-francis2011@hotmail.com>
2022-03-22 22:12:11 -0300

    add calculateComission of saller

---
+++ calculateCommission.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+  // Identificação do vendedor
+  // Código da peça
+  // Preço unitário da peça
+  // Quantidade vendida
+
+  char id[30];
+  int code_piece;
+  float price_piece;
+  float quantity_sold;
+
+  printf("What is your id? ");
+  scanf("%s", &id);
+
+  printf("What is the piece code? ");
+  scanf("%i", &code_piece);
+
+  printf("What is the price of the piece? ");
+  scanf("%f", &price_piece);
+
+  printf("What is the quantity of the piece? ");
+  scanf("%f", &quantity_sold);  
+
+  float totalValue = quantity_sold * price_piece;
+  totalValue *= 0.05;
+
+  printf("The id of saller %s who sold the piece %i ammounting to %.2f and the amount %.0f received the commission from %.2f", id, code_piece, price_piece, quantity_sold, totalValue);
+};
