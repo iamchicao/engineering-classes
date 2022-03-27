8239c446e57add56a44fcdf07c2cfe336e80118d
Francisco Junior <jr-francis2011@hotmail.com>
2022-03-27 13:00:16 -0300

    add salaryIncrease.c

---
+++ salaryIncrease.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+  // Declaring variables as floats
+  float salary, salary_increase, salary_increase_percentage;
+
+  // Data entry
+  printf("Enter your salary: ");
+  scanf("%f", &salary);
+
+  // Data processing to calculate the increase
+  if (salary <= 1500)
+  {
+    salary_increase_percentage = 0.35;
+  }
+  else
+  {
+    salary_increase_percentage = 0.15;
+  };
+
+  salary_increase = salary * salary_increase_percentage;
+  salary = salary + salary_increase;
+
+  // Data output
+  printf("Your salary is %.2f\n", salary);
+}
